d8b0b82455a939c53817d377df1e879b0378717a
Calvin <calvintallentspam@gmail.com>
2023-05-24 12:29:59 -0700

    Stuff.

---
+++ ArrayListDemonstration/President.java
@@ -0,0 +1,27 @@
+package ArrayListDemonstration;
+
+public class President {
+    private int number; 
+    private String name;
+
+    public President(int number, String name){
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
